3c47d467f2bfd474d17c0e65f041a7061eeca3e7
jorgiom4 <gorkalqy@hotmail.com>
2018-05-23 21:04:45 +0200

    Un commit

---
+++ src/com/instancia2/Main.java
@@ -0,0 +1,13 @@
+package com.instancia2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo desde el proyecto de pruebas con repositorio");
+        System.out.println("Esto es otra linea");
+    }
+
+    static void UnaFuncion(){
+        System.out.println("Otra salida por consola");
+    }
+}
