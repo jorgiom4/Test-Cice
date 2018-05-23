2d2bcd417dcfc18aa81cf55402b8dfed8c777c14
jorgiom4 <gorkalqy@hotmail.com>
2018-05-23 21:14:38 +0200

    Un commit nuevo

--- src/com/instancia2/Main.java
+++ src/com/instancia2/Main.java
@@ -5,9 +5,17 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hola mundo desde el proyecto de pruebas con repositorio");
         System.out.println("Esto es otra linea");
+
+        UnaFuncion();
     }
 
     static void UnaFuncion(){
         System.out.println("Otra salida por consola");
+        for(int i = 0; i<10; i++){
+            System.out.println("Valor de i: " + i);
+        }
+
+        System.out.println("Despues del for");
+
     }
 }
